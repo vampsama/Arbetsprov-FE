01bdaf3b8c7bd9d6a4466755913cb694d0176e97
John Persson <john.persson@isotop.se>
2021-10-24 00:24:10 +0200

    css fixes to go

---
+++ arbetsprov-fe/src/components/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import SearchField from "./SearchField";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="headerContainer">
+        <h1>Hur är vädret i...</h1>
+      </div>
+      <SearchField />
+    </div>
+  );
+}
+
+export default App;
