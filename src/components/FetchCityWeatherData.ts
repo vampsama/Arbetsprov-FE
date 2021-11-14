06b42bf99b62dbb8262b5d8ddbe26b8dba906bc3
John Persson <john.persson@isotop.se>
2021-11-14 14:47:14 +0100

    ny apinyckel

--- src/components/FetchCityWeatherData.ts
+++ src/components/FetchCityWeatherData.ts
@@ -1,7 +1,8 @@
 import { CityWeatherData } from "./CityWeatherData";
 
 //const ACCESS_KEY = "2a3c7d622ca26f8553a04f9d42aafdf2"; // old
-const ACCESS_KEY = "64a205ab0a9becd5365ed9239aa4885c";
+//const ACCESS_KEY = "64a205ab0a9becd5365ed9239aa4885c";
+const ACCESS_KEY = "dcfcf590912ddb7099a5ad1dbb75f2bd";
 
 const fetchCityWeatherData = async (city: string): Promise<any> => {
   const fetchURI = `http://api.weatherstack.com/current?access_key=${ACCESS_KEY}&query=${city}`;
